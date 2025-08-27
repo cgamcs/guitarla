4fdfec2baf4a45def248e420fc35ba4ddae2724b
César García <cesar.garciamcs@outlook.com>
2025-08-27 11:19:07 -0600

    agregando almacenamiento del carrito en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,21 @@ import Guitar from "./components/Guitar"
 import { db } from "./data/db"
 
 function App() {
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
   const [data, setData] = useState(db)
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(initialCart)
 
   const MAX_ITEMS = 5
   const MIN_ITEMS = 1
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item) {
     const itemExist = cart.findIndex(guitar => guitar.id === item.id)
