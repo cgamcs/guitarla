e3b044bee831d800c6a4c582ec68d7bfae74407d
César García <cesar.garciamcs@outlook.com>
2025-08-27 14:29:43 -0600

    creando un custom hook para el carrito

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,6 @@
 
 function Guitar({guitar, addToCart}) {
-    const {id, name, description, image, price} = guitar
+    const { name, description, image, price} = guitar
 
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
